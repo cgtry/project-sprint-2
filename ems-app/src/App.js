e5d4a1db8c246934a7bb295d713c468d27bfcf1a
amgrsmsport <56305816+amgrsmsport@users.noreply.github.com>
2021-05-31 09:05:39 +0530

    react

--- ems-app/src/App.js
+++ ems-app/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(100);
+  const increment = () => setCounter(counter + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter Demo</h1>
+      <div> {counter} </div>
+      <div>
+        <input type="button" value="Counter Option 1" onClick={increment} />
+      </div>
     </div>
   );
 }
