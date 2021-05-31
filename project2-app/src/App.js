f30e25f2181edc565b94331da0b75a38f163cd3b
amgrsmsport <56305816+amgrsmsport@users.noreply.github.com>
2021-05-31 21:36:22 +0530

    b3

--- project2-app/src/App.js
+++ project2-app/src/App.js
@@ -1,32 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [list] = useState([1, 1, 1, 1, 1]);
   return (
-    <div style={{ background: "#FF9900", height: "100vh" }}>
-      <h1 style={{ background: "tomato", color: "white" }}>Project Title</h1>
+    <div>
+      <h1 className="bg-danger text-light p-3 sticky-top fs-1 d-flex justify-content-center">
+        Mini Instagram
+      </h1>
 
-      <div style={{ fontFamily: "cursive" }}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ad
-        enim quis cum sed explicabo eos quidem ullam similique harum odit
-        reprehenderit repudiandae maxime error, vel voluptatibus incidunt?
-        Temporibus, nulla.
-      </div>
-
-      <h2 style={{ background: "tomato", color: "white" }}>Second Heading</h2>
-      <div style={{ fontFamily: "cursive" }}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates
-        deleniti quod ratione, eveniet reiciendis rem optio iste ipsum provident
-        doloremque quae porro. Magni, blanditiis? Rem laboriosam ipsum aut
-        incidunt ea?
-      </div>
-      <h2 style={{ background: "tomato", color: "white" }}>Third Heading</h2>
-      <div style={{ fontFamily: "cursive" }}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias cumque
-        laborum est! Placeat at ut ipsam! Porro adipisci expedita iste aperiam
-        dolorum esse vero impedit, necessitatibus voluptates similique voluptas
-        amet.
-      </div>
+      {list.map((item, index) => (
+        <div
+          key={index}
+          className="alert-secondary
+           m-3 rounded"
+          style={{ height: "300px" }}
+        >
+          <img
+            src={"https://picsum.photos/300" + index}
+            className="rounded"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            alt=""
+          />
+        </div>
+      ))}
     </div>
   );
 }
